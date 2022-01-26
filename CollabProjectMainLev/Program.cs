5743f9fe79b66585908572ff9ba23b2c7017f2c9
Viktor <viktor.kononenko.2003@gmail.com>
2022-01-26 11:30:27 +0500

    added func in a

--- CollabProjectMainLev/Program.cs
+++ CollabProjectMainLev/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public int EnterA()
+        {
+            int a = 0;
+            return a = int.Parse(Console.ReadLine());
+        }
+    }
+}
