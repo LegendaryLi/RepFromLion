66caa5fcf15482fbd3b82ac202eafed16ec706ea
Viktor <viktor.kononenko.2003@gmail.com>
2022-01-26 11:45:35 +0500

    added func in mult

--- CollabProjectMainLev/Program.cs
+++ CollabProjectMainLev/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public int Mult()
+        {
+            return EnterA() * EnterB();
+        }
+    }
+}
